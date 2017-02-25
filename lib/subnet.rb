4366f8f820bbcb2e88b6b5fb91b126313afc4734
Peter McGarrigle <peter.mcgarrigle@me.com>
2017-02-25 12:37:28 +0000

    initial

---
+++ lib/subnet.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+class Subnet
+
+  def name n
+     puts "name #{n}"
+  end
+
+  def ip4 n
+     puts "network #{n}"
+  end
+
+  def guest n
+     puts "guest #{n}"
+  end
+
+end
+
+def domain d
+  puts "domain #{d}"
+end
+
+def subnet(&block) 
+  s = Subnet.new
+  s.instance_eval &block
+end
+
+eval(File.read("network"))
+
