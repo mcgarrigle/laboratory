5b22fde8be180e74bac1fe5f53879f7defe43dd9
Peter McGarrigle <pete@iMac.local>
2017-02-26 11:38:29 +0000

    booting beachhead

--- beachhead-vm.sh
+++ beachhead-vm.sh
@@ -1,21 +1,23 @@
-cd ~/VirtualBox\ VMs/
-
 # Change these variables as needed
-VM_NAME="UbuntuServer"
-UBUNTU_ISO_PATH=~/Downloads/ubuntu-14.04.1-server-amd64.iso
-VM_HD_PATH="UbuntuServer.vdi" # The path to VM hard disk (to be created).
+
+VM_NAME="beachhead"
+ISO_PATH="${HOME}/Downloads/CentOS-7-x86_64-Everything-1511.iso"
+ISO_PATH="${HOME}/Downloads/CentOS-7-x86_64-Minimal-1511.iso"
+VM_HD_PATH="${VM_NAME}.vdi" # The path to VM hard disk (to be created).
 SHARED_PATH=~ # Share home directory with the VM
 
 
-vboxmanage createvm --name $VM_NAME --ostype Ubuntu_64 --register
+vboxmanage createvm --name $VM_NAME --ostype "RedHat_64" --register
 vboxmanage createhd --filename $VM_NAME.vdi --size 32768
 vboxmanage storagectl $VM_NAME --name "SATA Controller" --add sata --controller IntelAHCI
 vboxmanage storageattach $VM_NAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM_HD_PATH
 vboxmanage storagectl $VM_NAME --name "IDE Controller" --add ide
-vboxmanage storageattach $VM_NAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium $UBUNTU_ISO_PATH
+vboxmanage storageattach $VM_NAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium $ISO_PATH
 vboxmanage modifyvm $VM_NAME --ioapic on
 vboxmanage modifyvm $VM_NAME --memory 1024 --vram 128
 vboxmanage modifyvm $VM_NAME --nic1 nat
 vboxmanage modifyvm $VM_NAME --natpf1 "guestssh,tcp,,2222,,22"
 vboxmanage modifyvm $VM_NAME --natdnshostresolver1 on
 vboxmanage sharedfolder add $VM_NAME --name shared --hostpath $SHARED_PATH --automount
+
+vboxmanage startvm $VM_NAME
