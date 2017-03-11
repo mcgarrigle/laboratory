6d4baf28c3fcfda35b532204c06c379549e9fa74
Peter McGarrigle <mcgarrigle@github.com>
2017-03-11 12:25:56 +0000

    more vbox specs

--- spec/vbox_spec.rb
+++ spec/vbox_spec.rb
@@ -3,6 +3,9 @@ require "vbox"
 
 describe Vbox do
 
+  # string:
+  # takes a scalar and adds quotes if a string
+
   describe "#string" do
     
     it "returns quotes for strings" do
@@ -15,6 +18,18 @@ describe Vbox do
 
     it "returns string for numeric" do
       expect(subject.string(123)).to be == '123'
+    end
+
+  end
+
+  # flatten:
+  # take hash of and convert into commandline args
+
+  describe "#flatten" do
+    
+    it "returns correct types" do
+      args = { :string => "foo", :symbol => :bar, :integer => 123 }
+      expect(subject.flatten(args)).to be == '--string "foo" --symbol bar --integer 123'
     end
 
   end
