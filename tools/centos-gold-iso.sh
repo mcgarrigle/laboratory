9d068778f9d4c4297ac519df037ad5a83f9f62ad
mcgarrigle <peter.mcgarrigle@me.com>
2017-05-01 22:32:30 +0100

    Update centos-gold-iso.sh

--- tools/centos-gold-iso.sh
+++ tools/centos-gold-iso.sh
@@ -6,4 +6,4 @@ shopt -s dotglob
 cp -ri /media/cdrom/* /iso
 cp ../ks/gold.ks /iso/
 vi /iso/isolinux/isolinux.cfg 
-mkisofs -o /tmp/centos-gold-7,3,1611.iso -b isolinux/isolinux.bin -c isolinux/boot.cat --no-emul-boot --boot-load-size 4 --boot-info-table -J -R -V Gold .
+mkisofs -o /tmp/centos-gold-7.3.1611.iso -b isolinux/isolinux.bin -c isolinux/boot.cat --no-emul-boot --boot-load-size 4 --boot-info-table -J -R -V Gold .
