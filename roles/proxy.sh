58ff4ca5ce5031ee6e942c1d0b6c98dedb382cc8
Peter McGarrigle <mcgarrigle@github.com>
2017-05-29 21:50:48 +0100

    playpen => laboratory cull

--- roles/proxy.sh
+++
@@ -1,6 +0,0 @@
-
-# copy into /etc/profile.d/
-
-export http_proxy="http://10.0.30.10:9090"
-export https_proxy="$http_proxy"
-export no_proxy="localhost,127.0.0.1,.foo.local"
--- roles/sync-centos.sh
+++
@@ -1,4 +0,0 @@
-
-
-rsync  -avSHP --delete --exclude "local*" --exclude "isos" rsync://anorien.csc.warwick.ac.uk/CentOS/7.3.1611 /var/www/html/
-
