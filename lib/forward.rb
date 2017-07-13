8f97618b02067baab633044dfddce727b21cceb7
Peter McGarrigle <mcgarrigle@github.com>
2017-07-13 08:08:23 +0100

    more tests for forward

--- lib/forward.rb
+++ lib/forward.rb
@@ -4,10 +4,12 @@ class Forward
   attr_accessor :protocol, :from_ip, :from_port, :to_ip, :to_port
 
   def initialize(name, options)
+    @name     = name
+    @protocol = :tcp
   end
 
   def to_s
-    "ssh,tcp,127.0.0.1,2022,10.0.30.100,22"
+    "#{@name},#{@protocol},127.0.0.1,2022,10.0.30.100,22"
   end
 
 end
--- spec/forward_spec.rb
+++ spec/forward_spec.rb
@@ -5,12 +5,25 @@ describe Forward do
 
   describe "#to_s" do
 
-   subject { Forward.new("ssh", "127.0.0.1:2022" => "10.0.30.100:22") }
+   context "when fully described" do
 
-    it "should return " do
-      expect(subject.to_s).to eql "ssh,tcp,127.0.0.1,2022,10.0.30.100,22"
+     subject { Forward.new("ssh", :protocol => :tcp, "127.0.0.1:2022" => "10.0.30.100:22") }
+
+      it "should return netpf string" do
+        expect(subject.to_s).to eql "ssh,tcp,127.0.0.1,2022,10.0.30.100,22"
+      end
+
     end
 
+   context "when defaulting protocol" do
+
+     subject { Forward.new("ssh", "127.0.0.1:2022" => "10.0.30.100:22") }
+
+      it "should return netpf string" do
+        expect(subject.to_s).to eql "ssh,tcp,127.0.0.1,2022,10.0.30.100,22"
+      end
+
+    end
   end
 
 end
