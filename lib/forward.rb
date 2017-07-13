3ac80316ffa5a28e76e99c6e974fb1ee8cf5223c
Peter McGarrigle <peter.mcgarrigle@bt.com>
2017-07-13 10:27:53 +0100

    forward complete

--- lib/forward.rb
+++ lib/forward.rb
@@ -1,15 +1,33 @@
 
+require 'ipaddr'
+
+
 class Forward
 
   attr_accessor :protocol, :from_ip, :from_port, :to_ip, :to_port
 
-  def initialize(name, options)
+  def initialize(name, protocol: :tcp, from:, to:)
     @name     = name
-    @protocol = :tcp
+    @protocol = protocol
+    (@from_ip, @from_port) = endpoint(from)
+    (@to_ip, @to_port) = endpoint(to)
+  end
+
+  def ipaddr(s)
+    return "" if s.empty?
+    ip = IPAddr.new(s)
+    return s
+  end
+
+  def endpoint(s)
+    ip, port = s.split(":",2)
+    ip = ipaddr(ip)
+    return [ip, port]
   end
 
   def to_s
-    "#{@name},#{@protocol},127.0.0.1,2022,10.0.30.100,22"
+    # "#{@name},#{@protocol},127.0.0.1,2022,10.0.30.100,22"
+    "#{@name},#{@protocol},#{@from_ip},#{@from_port},#{@to_ip},#{@to_port}"
   end
 
 end
--- spec/forward_spec.rb
+++ spec/forward_spec.rb
@@ -5,25 +5,57 @@ describe Forward do
 
   describe "#to_s" do
 
-   context "when fully described" do
+    context "when fully described" do
 
-     subject { Forward.new("ssh", :protocol => :tcp, "127.0.0.1:2022" => "10.0.30.100:22") }
-
-      it "should return netpf string" do
-        expect(subject.to_s).to eql "ssh,tcp,127.0.0.1,2022,10.0.30.100,22"
-      end
-
-    end
-
-   context "when defaulting protocol" do
-
-     subject { Forward.new("ssh", "127.0.0.1:2022" => "10.0.30.100:22") }
+      subject { Forward.new("ssh", protocol: :tcp, from:"127.0.0.1:2022", to:"10.0.30.100:22") }
 
       it "should return netpf string" do
         expect(subject.to_s).to eql "ssh,tcp,127.0.0.1,2022,10.0.30.100,22"
       end
 
     end
-  end
 
-end
+    context "when including badly formed address" do
+
+      subject { Forward.new("ssh", protocol: :tcp, from:"XXX.0.0.1:2022", to:"10.0.30.100:22") }
+
+      it "should error" do
+        expect { subject }.to raise_error IPAddr::InvalidAddressError
+      end
+
+    end
+
+    context "when defaulting protocol" do
+
+      subject { Forward.new("ssh", from:"127.0.0.1:2022", to:"10.0.30.100:22") }
+
+      it "should return netpf string" do
+        expect(subject.to_s).to eql "ssh,tcp,127.0.0.1,2022,10.0.30.100,22"
+      end
+
+   end
+
+    context "when defaulting addresses" do
+
+      subject { Forward.new("ssh", from:":2022", to:":22") }
+
+      it "should return netpf string" do
+        expect(subject.to_s).to eql "ssh,tcp,,2022,,22"
+      end
+
+    end
+
+    context "when setting protocol" do
+
+      subject { Forward.new("ssh", protocol: :udp, from:"127.0.0.1:2022", to:"10.0.30.100:22") }
+
+      it "should return netpf string" do
+         expect(subject.to_s).to eql "ssh,udp,127.0.0.1,2022,10.0.30.100,22"
+      end
+
+    end # context
+
+  end # describe
+
+end # Forward
+
