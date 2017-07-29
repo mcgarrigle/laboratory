9cf29e78627afcb8b42924a9bf33b462071bf004
Peter McGarrigle <peter.mcgarrigle@me.com>
2017-07-29 21:55:46 +0100

    renamed network.lab

--- lib/laboratory.rb
+++ lib/laboratory.rb
@@ -1,5 +1,6 @@
 
 require "netaddr"
+require "network"
 require "guest"
 
 
@@ -8,8 +9,9 @@ class Laboratory
   attr_accessor :name, :guests
 
   def initialize(name = "")
-    @name = name
-    @guests = []
+    @name     = name
+    @networks = []
+    @guests   = []
   end
 
   def ip4
@@ -20,6 +22,10 @@ class Laboratory
     @network = NetAddr::CIDR.create(cidr)   # define subnet
     @pool = @network.enumerate              # create pool of addresses
     @pool.shift 10                          # first 10 IPs are reserved 
+  end
+
+  def network(name, options = {})
+    @networks << Network.new(name, options)
   end
 
   def guest(name = nil)
