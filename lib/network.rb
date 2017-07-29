2f45c5e744f967f620e281892a500c050c382455
Peter McGarrigle <mcgarrigle@github.com>
2017-07-29 20:37:30 +0100

    check network arguments

--- lib/network.rb
+++ lib/network.rb
@@ -8,10 +8,15 @@ class Network
   def initialize(name, options = {})
     @name = name.to_s 
     @connection, address = options.first
+    assert(@connection, :nat, :intnet, :hostonly)
     cidr = NetAddr::CIDR.create(address)
     @ip4      = cidr.ip
     @netmask4 = cidr.netmask_ext
     @prefix4  = cidr.netmask
+  end
+
+  def assert(value, *valid)
+    raise "#{value} is an incorrect option" unless (valid.include? value)
   end
 
 end
