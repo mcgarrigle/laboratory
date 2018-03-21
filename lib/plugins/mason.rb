1b7d3e07064f47b813281953f166a00402194dbe
Peter McGarrigle <mcgarrigle@github.com>
2018-03-21 18:36:40 +0000

    mason plugin serialises guest

--- lib/plugins/mason.rb
+++ lib/plugins/mason.rb
@@ -1,14 +1,22 @@
 
+require 'net/http'
+require 'json'
+
 module Plugins
 
   class Mason
 
-    def initialize(options = {})
-      #puts "mason: init"
+    def initialize(options = {:api => "http://localhost:9090"})
+      @uri = URI(options[:api])
     end
 
     def create(guest)
-      #puts "mason: create #{guest.name}"
+      puts "mason: create #{guest.name}"
+      interfaces = guest.interfaces.map {|i| { :mac => i.mac, :ip => i.ip4, :netmask => i.netmask4 } }
+      hash = {:fqdn => guest.name, :interfaces => interfaces }
+      res = Net::HTTP.start(@uri.hostname, @uri.port) { |http|
+        http.put("/node/#{guest.name}", hash.to_json)
+      }
     end
 
     def start(guest)
